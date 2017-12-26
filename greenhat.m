f794b8df31e8f6a503b4fc8f0ea78dae62bb1dfe
Zhi Huang <zhi@Zhis-MacBook-Pro.local>
2017-12-25 22:44:50 -0600

    init

--- greenhat.m
+++ greenhat.m
@@ -1,1 +1,19 @@
 % Greenhat
+close all; clear all; clc;
+%%
+%To detect Face
+FDetect = vision.CascadeObjectDetector;
+
+%Read the input image
+I = imread('lenna.jpeg');
+
+%Returns Bounding Box values based on number of objects
+BB = step(FDetect,I);
+
+figure
+imshow(I); hold on
+for i = 1:size(BB,1)
+    rectangle('Position',BB(i,:),'LineWidth',5,'LineStyle','-','EdgeColor','r');
+end
+title('Face Detection');
+hold off;
