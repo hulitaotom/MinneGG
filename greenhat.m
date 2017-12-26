922c1c94dab768b365fb312243fcdf49b000e40b
Zhi Huang <zhi@Zhis-MacBook-Pro.local>
2017-12-25 23:18:56 -0600

    init

--- greenhat.m
+++ greenhat.m
@@ -6,9 +6,18 @@ FDetect = vision.CascadeObjectDetector;
 
 %Read the input image
 I = imread('lenna.jpeg');
-
+hat = imread('greenhat.png');
 %Returns Bounding Box values based on number of objects
 BB = step(FDetect,I);
+
+hat = imresize(hat, [BB(3) BB(3)*2]);
+hatmask = I.*0;
+hatmask(BB(1):(BB(1)+BB(3)-1), (BB(2)-BB(3)):(BB(2)+BB(3)-1), :) = hat;
+Ihat = I + hatmask.*2;
+imshow(Ihat)
+
+%%
+
 
 figure
 imshow(I); hold on
